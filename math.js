6858a53bafac935d336c98b673d617443db2e39a
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-03-23 07:45:25 +0530

    Initial commit

---
+++ math.js
@@ -0,0 +1,11 @@
+const pi = 3.15931;
+
+function double() {
+  return pi * 2;
+}
+
+function triple() {
+  return pi * 3;
+}
+export default pi;
+export { double, triple };
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/* import pi, { double, triple } from "/math.js"; */
+import * as pi from "/math.js";
+ReactDOM.render(
+  <ul>
+    <li>{pi.default}</li>
+    <li>{pi.double()}</li>
+    <li>{pi.triple()}</li>
+  </ul>,
+  document.getElementById("root")
+);
